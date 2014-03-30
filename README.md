f1d9f40f9451b4fe1b003438e5b6fadca5785e33
felics <fspoettel@gmail.com>
2014-03-30 13:05:13 +0200

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Fundament Grid System
+
+A flexbox-first, inline-block-fallback, semantic and customizable scss grid system. Started as a fork of [css-wizardry-grids](https://github.com/csswizardry/csswizardry-grids). Works & tested in all major browsers. (IE8 with Respond.js)
+
+Features:
+
+ - Uses the new flexbox-syntax where possible, falls back to a solid `display: inline-block` based grid
+ - Fully customizable, including the ability to choose a namespace for grid-classes
+ - Mobile first & responsive if needed
+ - Can be used with silent classes & extends to minimize code-bloat
+
+## Requirements
+
+ - SASS 3.3: This project makes extensive use of SASS Maps, which were introduced in the new SASS 3.3.
+
+If you want to build the grid with the build-process included, you need to have `node.js` and `npm` installed. You can init the project like this:
+
+```
+npm install
+gulp
+```
+
+## Documentation
+
+Coming soon, hang in tight.
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+
+// *    Gulpfile
+// * ---------------------
+// * This file contains the gulp build-process.
+// * ---------------------
+
+// *   Core
+// * ---------------------
+
+// * Require gulp modules
+
+var gulp          = require("gulp"),
+    cssmin        = require("gulp-minify-css"),
+    plumber       = require("gulp-plumber"),
+    rename        = require("gulp-rename"),
+    sass          = require("gulp-ruby-sass");
+
+// * scss
+// * 1. handle errors
+// * 2. sass
+// * 3. prefix
+// * --> write out
+// * 4. minify
+// * --> write out
+// * ---------------------
+
+gulp.task("sass", function () {
+  gulp.src("fundament.scss")
+    .pipe(sass({noCache:true}))
+    .pipe(rename("fundament.css"))
+    .pipe(gulp.dest("dist/"))
+    .pipe(cssmin())
+    .pipe(rename("fundament.min.css"))
+    .pipe(gulp.dest("dist/"));
+});
+
+
+// * Tasks
+// * ---------------------
+
+// * Default task
+
+gulp.task("default", ["sass"]);

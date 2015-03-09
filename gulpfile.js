ce533d0963caea226fe09e265ce74a7e6b66098f
felics <fspoettel@gmail.com>
2015-03-09 22:48:38 +0100

    Use @mixin instead of @extend
    Synthax flex

--- gulpfile.js
+++ gulpfile.js
@@ -26,16 +26,28 @@ var gulp   = require("gulp"),
 gulp.task("sass", function () {
   return sass("scss/fundament-grid.scss", {sourcemap: false})
     .pipe(prefix({
-        browsers: ["last 1 version", "Explorer >= 10", "Android >= 4.4"]
+        browsers: ["last 2 versions", "Explorer >= 10", "Android >= 4.4"]
     }))
     .pipe(rename("fundament-grid.css"))
     .pipe(gulp.dest("./dist"));
 });
 
+gulp.task("test", function () {
+  return sass("test/css/test.scss", {sourcemap: false})
+    .pipe(prefix({
+        browsers: ["last 2 versions", "Explorer >= 10", "Android >= 4.4"]
+    }))
+    .pipe(gulp.dest("./test/css"));
+});
 
 // * Tasks
 // * ---------------------
 
 // * Default task
 
-gulp.task("default", ["sass"]);
+gulp.task("default", ["watch"]);
+
+
+gulp.task("watch", ["sass"], function(){
+    gulp.watch(["./scss/*.scss", "./scss/**/*.scss"], ["sass"])
+})

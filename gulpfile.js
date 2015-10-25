d62794a45c84145aa8a60899b6c664c70803767d
Felix S <felix.spoettel@fu-berlin.de>
2015-10-25 14:45:14 +0100

    Consistent Codestyle

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,32 @@
 // *   Core
 // * ---------------------
 
+'use strict';
+
 // * Require gulp modules
 
-var gulp   = require("gulp");
-var rename = require("gulp-rename");
-var prefix = require("gulp-autoprefixer");
-var sass   = require("gulp-sass");
-var jade   = require("gulp-jade");
+const gulp   = require('gulp');
+const jade   = require('gulp-jade');
+const prefix = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const sass   = require('gulp-sass');
 
-gulp.task("sass", function() {
-  return gulp.src("./__test__/src/scss/test.scss")
-    .pipe(sass().on("error", sass.logError))
+gulp.task('sass', function() {
+  return gulp.src('./__test__/src/scss/test.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(prefix({
-      browsers: ["last 2 versions", "Explorer >= 10", "Android >= 4.4"],
+      browsers: ['last 2 versions', 'Explorer >= 10', 'Android >= 4.4'],
     }))
-    .pipe(rename("styles.css"))
-    .pipe(gulp.dest("./__test__/css"));
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('./__test__/css'));
 });
 
-gulp.task("jade", function() {
-  return gulp.src("./__test__/src/jade/*.jade")
+gulp.task('jade', function() {
+  return gulp.src('./__test__/src/jade/*.jade')
     .pipe(jade({
       pretty: true,
     }))
-    .pipe(gulp.dest("./__test__/"));
+    .pipe(gulp.dest('./__test__/'));
 });
 
 // * Tasks
@@ -38,19 +40,19 @@ gulp.task("jade", function() {
 
 // * Default task
 
-gulp.task("default", ["watch"]);
+gulp.task('default', ['watch']);
 
-gulp.task("watch", ["sass", "jade"], function() {
-
-  gulp.watch([
-    "./src/*.scss", "./src/**/*.scss",
-    "./__test__/src/scss/test.scss",
-    "./__test__/src/scss/**/*.scss",
-  ], ["sass"]);
+gulp.task('watch', ['sass', 'jade'], function() {
 
   gulp.watch([
-    "./__test__/src/jade/*.jade",
-    "./__test__/src/jade/*/**.jade",
-  ], ["jade"]);
+    './src/*.scss', './src/**/*.scss',
+    './__test__/src/scss/test.scss',
+    './__test__/src/scss/**/*.scss',
+  ], ['sass']);
+
+  gulp.watch([
+    './__test__/src/jade/*.jade',
+    './__test__/src/jade/*/**.jade',
+  ], ['jade']);
 
 });
